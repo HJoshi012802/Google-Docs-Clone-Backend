30fa6b3c5e03e648070e084cf7fbfac9ef282fc6
HJoshi012802 <harshitjoshi2002@gmail.com>
2024-12-13 01:42:09 +0530

    socket documentId Logic written

--- index.js
+++ index.js
@@ -27,18 +27,21 @@ app.get('/', (req, res) => {
 
  io.on("connection",(socket) =>{
     console.log("CONNECTION FROM CLIENT !!! :" ,socket.id);
-    socket.on("send-quill-changes",delta =>{
-        console.log(delta);
-        socket.broadcast.emit("recive-quill-changes",delta);
-    })
 
+    socket.on('send-documentId',(documentId)=>{
+        const data = ""
+        socket.join(documentId);
+        socket.emit('load-document',data);
+        socket.on("send-quill-changes",delta =>{
+            console.log(delta);
+            socket.broadcast.to(documentId).emit("recive-quill-changes",delta);
+        })
+    })
+    
     socket.on("disconnect",()=>{
     console.log("DISCONNECTED FROM CLIENT !!! :" ,socket.id);
    })
  })
-//  io.on("disconnect",(socket)=>{
-//     console.log("DISCONNECTED FROM CLIENT !!! :" ,socket.id);
-//  })
 
   server.listen(6069, () => {
     console.log('Server is running on port 6069');
